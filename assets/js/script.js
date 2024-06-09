127b3ec0c7e2f59b2570764cfc1d3cb2c5a93af9
Farha Islam <farha.islam@students.codeinstitute.net>
2024-06-09 10:17:09 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,10 @@ function runGame(gameType) {
     else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
     } 
+
+    else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    } 
     
     else {
         alert(`Unknown game type: ${gameType}`);
@@ -75,6 +79,11 @@ function calculateCorrectAnswer() {
     else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     }
+
+    else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    }
+
     else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}.Aborting!`;
@@ -107,9 +116,10 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
-
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
